4090e6c4b837d3bd53b17dd5d2d7dc54641c780c
VolundRing <nvieland@UCSD.EDU>
2018-02-01 14:07:47 -0800

    DeskandChairColidersandScale

---
+++ Assets/materialScaler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+	void Start() {
+		Texture myTexture = Resources.Load("Wood") as Texture;    
+		MeshRenderer thisMesh = gameObject.GetComponent<MeshRenderer> ();
+		thisMesh.material.mainTextureScale = new Vector2(transform.lossyScale.x*0.5f,transform.lossyScale.y*0.5f);       
+
+	}
+}
