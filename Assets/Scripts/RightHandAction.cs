301e526e6dd2c5205e0f253f80f49e9a85679bd1
VolundRing <nvieland@UCSD.EDU>
2018-02-05 15:18:30 -0800

    Right Hand Complete

--- Assets/Scripts/RightHandAction.cs
+++ Assets/Scripts/RightHandAction.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class RightHandAction : MonoBehaviour {
 	public LineRenderer raycast;
 	GameObject chosen;
+	public SphereCollider thisCollider;
 	Transform chosenparent;
 	bool isGrabbed = false;
+	//bool hasEntered = false;
+	float teleportTimer = 4f;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,30 +22,62 @@ public class RightHandAction : MonoBehaviour {
 			RaycastHit hit;
 			Vector3 fwd = transform.TransformDirection (Vector3.forward);
 			Vector3 endPosition = transform.position;
-			if (isGrabbed == false) {
-				if (Physics.Raycast (transform.position, fwd, out hit, 10000)) {
-					endPosition = hit.point;
-					chosen = hit.collider.gameObject;
-					if (chosen.GetComponent<Rigidbody> () != null) {
-						if (OVRInput.Get (OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.Touch) == 1.0f) {
+			if (Physics.Raycast (transform.position, fwd, out hit, 10000)) {
+				endPosition = hit.point;
+				chosen = hit.collider.gameObject;
+				if (chosen.GetComponent<Rigidbody> () != null) {
+					if (OVRInput.Get (OVRInput.Button.SecondaryHandTrigger, OVRInput.Controller.Touch) == true) {
+						if (isGrabbed == false) {
 							isGrabbed = true;
 							chosen.GetComponent<Rigidbody> ().isKinematic = true;
 							chosenparent = chosen.transform.parent;
 							chosen.transform.parent = transform;
 							//chosen.transform.position = transform.position - transform.forward;
-						} else {
-							isGrabbed = false;
-							chosen.transform.parent = null;
-							chosen.GetComponent<Rigidbody> ().isKinematic = false;
+						}
+					} else {
+						isGrabbed = false;
+						chosen.transform.parent = null;
+						chosen.GetComponent<Rigidbody> ().isKinematic = false;
+						//chosen = null;
+					}
+				} else if (chosen.GetComponent<TerrainCollider> () != null) {
+					if (OVRInput.Get (OVRInput.Button.One, OVRInput.Controller.Touch) == true) {
+						teleportTimer -= Time.deltaTime;
+						if (teleportTimer < 0) {
+							GameObject.Find("OVRPlayerController").transform.position = hit.point;
+							teleportTimer = 4f;
 						}
 					}
-
 				}
+
 			}
 			raycast.SetPosition (0, transform.position);
 			raycast.SetPosition (1, endPosition);
 		} else {
 			raycast.enabled = false;
+
+		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (raycast.enabled == false) {
+			chosen = other.gameObject;
+			if (chosen.GetComponent<Rigidbody> () != null) {
+				if (OVRInput.Get (OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.Touch) == 1.0f) {
+					if (isGrabbed == false) {
+						isGrabbed = true;
+						chosen.GetComponent<Rigidbody> ().isKinematic = true;
+						chosenparent = chosen.transform.parent;
+						chosen.transform.parent = transform;
+						//chosen.transform.position = transform.position - transform.forward;
+					}
+				} else {
+					isGrabbed = false;
+					chosen.transform.parent = null;
+					chosen.GetComponent<Rigidbody> ().isKinematic = false;
+				}
+			}
 		}
 	}
 }
