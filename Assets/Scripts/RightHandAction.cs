6686e4eeba1dfd5136803f1bc753e31e51e9446f
Hemkinator <mhemker@UCSD.EDU>
2018-02-05 14:30:56 -0800

    Fixed? Raycast

--- Assets/Scripts/RightHandAction.cs
+++ Assets/Scripts/RightHandAction.cs
@@ -6,6 +6,7 @@ public class RightHandAction : MonoBehaviour {
 	public LineRenderer raycast;
 	GameObject chosen;
 	Transform chosenparent;
+	bool isGrabbed = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,21 +19,25 @@ public class RightHandAction : MonoBehaviour {
 			RaycastHit hit;
 			Vector3 fwd = transform.TransformDirection (Vector3.forward);
 			Vector3 endPosition = transform.position;
-			if (Physics.Raycast (transform.position, fwd, out hit, 10000)) {
-				endPosition = hit.point;
-				chosen = hit.collider.gameObject;
-				if (chosen.GetComponent<Rigidbody>() != null) {
-					if (OVRInput.Get (OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.Touch) == 1.0f) {
-						chosen.GetComponent<Rigidbody> ().isKinematic = true;
-						chosenparent = chosen.transform.parent;
-						chosen.transform.parent = transform;
-						//chosen.transform.position = transform.position - transform.forward;
-					} else {
-						chosen.transform.parent = null;
-						chosen.GetComponent<Rigidbody> ().isKinematic = false;
+			if (isGrabbed == false) {
+				if (Physics.Raycast (transform.position, fwd, out hit, 10000)) {
+					endPosition = hit.point;
+					chosen = hit.collider.gameObject;
+					if (chosen.GetComponent<Rigidbody> () != null) {
+						if (OVRInput.Get (OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.Touch) == 1.0f) {
+							isGrabbed = true;
+							chosen.GetComponent<Rigidbody> ().isKinematic = true;
+							chosenparent = chosen.transform.parent;
+							chosen.transform.parent = transform;
+							//chosen.transform.position = transform.position - transform.forward;
+						} else {
+							isGrabbed = false;
+							chosen.transform.parent = null;
+							chosen.GetComponent<Rigidbody> ().isKinematic = false;
+						}
 					}
-				}
 
+				}
 			}
 			raycast.SetPosition (0, transform.position);
 			raycast.SetPosition (1, endPosition);
