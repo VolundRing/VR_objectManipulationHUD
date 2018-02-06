936d06d8b95dd6c9544673540cd463933ed83144
VolundRing <nvieland@UCSD.EDU>
2018-02-05 16:53:51 -0800

    Mondayclass

--- Assets/spawn.cs
+++ Assets/spawn.cs
@@ -4,34 +4,55 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour {
 
+	public LineRenderer raycast;
 	public bool spwnMode = false ;
 	public static GameObject[] deckObjs;
 	public int whichDeckObj = 0;
+	public GameObject chosen;
 	public int PFarrayLength = 6;
 	private bool newRPress = false;
 	private bool newLPress = false;
 	private bool newObj = false;
+	public int groupLength = 0;
 	GameObject miniObj;
+	GameObject fullObj;
+	public List<GameObject> Group = new List<GameObject>();
+
 
 	void Start () {
 		
 	}
-
-	// Mode indicator
-	bool modeSwitch () {
-			return spwnMode;
-	}
-
-	int whichObj () {
-		return whichDeckObj;
-	}
+		
 		
 	void Update () {
+		
 		//Mode switching
 		if (spwnMode == false) {
+			RaycastHit hit;
+			Vector3 fwd = transform.TransformDirection (Vector3.forward);
+			Vector3 endPosition = transform.position;
+			if (Physics.Raycast (transform.position, fwd, out hit, 10000)) {
+				endPosition = hit.point;
+				chosen = hit.collider.gameObject;
+				if (chosen.GetComponent<Rigidbody> () != null) {
+					if (OVRInput.GetDown(OVRInput.Button.Three, OVRInput.Controller.Touch) == true) {
+						if (Group.Contains (chosen)) { 
+							Group.Remove (chosen);
+							groupLength = Group.Count;
+						} else {
+							Group.Add (chosen); 
+							groupLength = Group.Count;
+						}
+					}
+				}
+			}
+				
+
 			if (OVRInput.GetDown (OVRInput.Button.PrimaryThumbstick) == true) {
 				spwnMode = true ;
 			}
+			raycast.SetPosition (0, transform.position);
+			raycast.SetPosition (1, endPosition);
 		}
 		else {
 			if (OVRInput.GetDown (OVRInput.Button.PrimaryThumbstick) == true) {
@@ -43,7 +64,7 @@ public class spawn : MonoBehaviour {
 			//whichDeckObj incrementor
 			if (newRPress == false) {				
 				if (OVRInput.Get (OVRInput.Button.PrimaryThumbstickRight) == true) {
-					if (whichDeckObj < PFarrayLength) { 
+					if (whichDeckObj < deckObjs.Length) { 
 						whichDeckObj++;					
 					}
 					newRPress = true;
@@ -76,15 +97,26 @@ public class spawn : MonoBehaviour {
 				miniObj = Instantiate (deckObjs [whichDeckObj], transform.position, transform.rotation) as GameObject;
 				miniObj.transform.localScale = new Vector3 (0.2f, 0.2f, 0.2f);
 				miniObj.GetComponent<Rigidbody> ().isKinematic = true;
-				newObj = true;
+				miniObj.transform.parent = transform;
+
+			}
+			newObj = true;
+
+			if (OVRInput.GetDown (OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.Touch) == true) {
+				//Destroy (miniObj);
+				fullObj = Instantiate (deckObjs [whichDeckObj], new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.2f), transform.rotation) as GameObject;
+				fullObj.GetComponent<Rigidbody> ().isKinematic = true;
+				fullObj.transform.parent = transform;
+
+
+			}
+			if (OVRInput.GetUp (OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.Touch) == true) {
+				fullObj.GetComponent<Rigidbody> ().isKinematic = false;
+				fullObj.transform.parent = null;
 			}
 
-			miniObj.transform.parent = transform;
-		}
-	}
-	void MinInstan () {
-		if (spwnMode == true) {
 
 		}
 	}
 }
+
